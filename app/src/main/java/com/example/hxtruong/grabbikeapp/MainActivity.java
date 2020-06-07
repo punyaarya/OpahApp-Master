8d38494ebff7e84973bee9febf7efa20d0e852ac
Aryaabmy <aryaabmy@gmai.com>
2020-06-07 11:54:17 +0700

    first commit lagi

--- app/src/main/java/com/example/hxtruong/grabbikeapp/MainActivity.java
+++ app/src/main/java/com/example/hxtruong/grabbikeapp/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
